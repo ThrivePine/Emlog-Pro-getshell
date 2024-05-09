fe6d66f0223f3592bc3a56ef2e70e530bfbe989a
song <91169284+ThrivePine@users.noreply.github.com>
2024-05-09 21:12:09 +0800

    Add files via upload

---
+++ tips/tips_callback.php
@@ -0,0 +1,17 @@
+<?php
+!defined('EMLOG_ROOT') && exit('access deined!');
+
+// 开启插件时执行该函数
+function callback_init() {
+    // do something
+}
+
+// 删除插件时执行该函数
+function callback_rm() {
+    // do something
+}
+
+// 更新插件时执行该函数
+function callback_up() {
+    // do something
+}
